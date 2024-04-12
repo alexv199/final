44f625bfd58b2a2c8d5d3580dc8506e367da57db
Emile <enbymonkey@gmail.com>
2024-04-12 19:59:37 -0400

    upload 3

---
+++ project/events_dml/complete/25.sql
@@ -0,0 +1,19 @@
+INSERT INTO event_25 (event_id, match_id, index, period, timestamp, possession, possession_team_id, play_pattern, team_id, player_id, position, location) VALUES
+( "c56d7309-3a54-41f2-a058-4ab5fc49d721", 3749153, 1629, 1, "00:46:21.736", 116, 1, "From Free Kick", 1, null, null, [115.1, 42.1] ),
+( "cb02efe7-3568-4e83-9a2d-0797e9b76a1a", 3749454, 397, 1, "00:09:42.684", 32, 1, "Regular Play", 36, null, null, [106.4, 38.3] ),
+( "72d3f4f3-c66f-427d-8ec0-b1b978ec0f41", 3773403, 1996, 2, "00:03:26.025", 98, 215, "From Free Kick", 215, null, null, [108.4, 47.7] ),
+( "9157aac9-8ae3-4052-a8b5-aa597d9a353f", 3773466, 2280, 2, "00:05:20.137", 98, 217, "From Throw In", 217, null, null, [114.5, 41.9] ),
+( "b7d3aef0-8271-4103-9ea2-1decc3be4561", 303696, 436, 1, "00:10:57.283", 22, 217, "From Corner", 217, null, null, [117.2, 43.2] ),
+( "be9b2a4b-ac99-48cf-a4f8-9dab45939d45", 3749233, 1596, 1, "00:36:26.648", 73, 1, "Regular Play", 1, null, null, [119.6, 40.5] ),
+( "c901f74d-7d15-47e6-897c-18153b10757d", 16029, 3044, 2, "00:33:30.271", 195, 213, "Regular Play", 213, 6720, "Right Attacking Midfield", [109.0, 47.0] ),
+( "bd33f911-ca6c-4837-a086-cd3810a8307e", 16306, 4241, 2, "00:43:57.269", 155, 217, "From Free Kick", 217, 5503, "Right Center Forward", [120.0, 42.0] ),
+( "35b7d0ac-d24a-4cda-91b4-a8c38c42171a", 3749522, 529, 1, "00:12:47.023", 36, 1, "From Corner", 1, null, null, [114.8, 38.4] ),
+( "5a2dc053-5543-4093-9a8e-dc1d374c7d4f", 3749253, 1099, 1, "00:30:41.809", 65, 1, "From Free Kick", 1, null, null, [113.9, 37.0] ),
+( "b5dd0767-4e92-44d6-aaf7-024f4a9e6aba", 3773631, 2994, 2, "00:22:16.104", 124, 217, "From Free Kick", 217, null, null, [113.5, 35.8] ),
+( "8e612fba-aab2-4e63-ac7f-677b2a1674fb", 15973, 999, 1, "00:23:46.499", 47, 217, "From Throw In", 217, null, null, [115.7, 42.5] ),
+( "af105f4e-cee8-4103-923a-42df53381feb", 3773661, 438, 1, "00:11:39.894", 18, 216, "Regular Play", 216, null, null, [114.8, 43.1] ),
+( "5e0a2c0c-cba6-48fd-aa50-ae7703973a24", 3773661, 1232, 1, "00:27:16.858", 38, 216, "Regular Play", 217, null, null, [104.5, 36.7] ),
+( "44264393-9900-4d00-ab22-3e96c66e1d61", 3773593, 2072, 1, "00:45:00.538", 79, 217, "Regular Play", 217, null, null, [118.8, 39.5] ),
+( "a398ef3a-1077-4017-a5d3-dda492025e66", 303725, 158, 1, "00:02:56.145", 9, 217, "Regular Play", 217, null, null, [116.5, 35.2] ),
+( "76eab3b5-f5eb-4a49-8750-93d357ab05bd", 3773428, 2654, 2, "00:11:56.690", 100, 217, "Regular Play", 217, null, null, [114.1, 30.5] );
+
