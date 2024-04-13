bb9c099aad0e28419194b7ea91f692e3fcb91f18
Emile <enbymonkey@gmail.com>
2024-04-13 17:30:42 -0400

    update

---
+++ project/events_dml/20.sql
@@ -0,0 +1,19 @@
+INSERT INTO event_20 (event_id, match_id, index, period, timestamp, possession, possession_team_id, play_pattern, team_id, player_id, position, location) VALUES
+( '132c12b5-7c02-44b7-9bbf-ce1ca7f44330', 3749153, 1628, 1, '00:46:21.736', 116, 1, 'From Free Kick', 47, 40328, 'Left Back', '(5.0, 38.0)' ),
+( 'af9a2d58-be5f-459c-a226-dc3537d5735a', 3749454, 398, 1, '00:09:42.684', 32, 1, 'Regular Play', 1, 40222, 'Right Back', '(13.7, 41.8)' ),
+( '708c4a08-164e-403f-a099-5380fa023bef', 3773403, 1997, 2, '00:03:26.025', 98, 215, 'From Free Kick', 217, 5211, 'Left Back', '(11.7, 32.4)' ),
+( 'b2b975e8-b2f7-466f-8961-9b585e90fe6a', 3773466, 2281, 2, '00:05:20.137', 98, 217, 'From Throw In', 209, 24730, 'Left Wing Back', '(5.6, 38.2)' ),
+( 'f51a7bcc-f26f-414d-9886-e55213e4984e', 303696, 437, 1, '00:10:57.283', 22, 217, 'From Corner', 212, 5198, 'Left Center Forward', '(2.9, 36.9)' ),
+( '9055cabd-96b1-46ac-99c3-bedf55682b72', 3749233, 1595, 1, '00:36:26.648', 73, 1, 'Regular Play', 36, 24758, 'Left Back', '(0.5, 39.6)' ),
+( '80df3165-c94a-4bf4-a3c0-9d17eef5acc4', 16029, 3045, 2, '00:33:30.271', 195, 213, 'Regular Play', 217, 6826, 'Left Center Back', '(12.0, 34.0)' ),
+( '54058aa6-a883-484f-aa0b-7b40f558c4ab', 16306, 4240, 2, '00:43:57.269', 155, 217, 'From Free Kick', 216, 6863, 'Left Defensive Midfield', '(1.0, 39.0)' ),
+( 'c86ea6ba-24a9-46d1-824f-6e0752c049ea', 3749522, 530, 1, '00:12:47.023', 36, 1, 'From Corner', 46, 40314, 'Right Center Back', '(5.3, 41.7)' ),
+( '95dce250-b9e5-46fc-84f0-24f3ba9c396f', 3749253, 1100, 1, '00:30:41.809', 65, 1, 'From Free Kick', 24, 34389, 'Left Center Back', '(6.2, 43.1)' ),
+( 'd509f573-7247-433e-8e7b-a6ff0aca5b00', 3773631, 2995, 2, '00:22:16.104', 124, 217, 'From Free Kick', 218, 6752, 'Left Center Back', '(6.6, 44.3)' ),
+( 'd60fa02f-8f81-4bdc-bca8-a1e19624221a', 15973, 1000, 1, '00:23:46.499', 47, 217, 'From Throw In', 902, 11677, 'Right Center Back', '(4.4, 37.6)' ),
+( '7ad6c21f-b4a1-46cb-9755-1f74e41c3f7b', 3773661, 437, 1, '00:11:39.894', 18, 216, 'Regular Play', 217, 6826, 'Left Center Back', '(5.3, 37.0)' ),
+( 'dd075ff7-fc3e-46e9-98a9-68ca95d3c630', 3773661, 1233, 1, '00:27:16.858', 38, 216, 'Regular Play', 216, 35287, 'Left Center Back', '(15.6, 43.4)' ),
+( '64d52ae9-726e-4c70-a9b2-16b64b3d71ba', 3773593, 2071, 1, '00:45:00.538', 79, 217, 'Regular Play', 222, 6892, 'Left Center Back', '(1.3, 40.6)' ),
+( '7fcac7a3-a1b8-413b-9eac-ae743eee1a47', 303725, 159, 1, '00:02:56.145', 9, 217, 'Regular Play', 222, 6892, 'Left Center Back', '(3.6, 44.9)' ),
+( 'd565f788-a6e9-4155-8bb7-60c01ce08e42', 3773428, 2655, 2, '00:11:56.690', 100, 217, 'Regular Play', 552, 6579, 'Right Center Back', '(6.0, 49.6)' );
+
